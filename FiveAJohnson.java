0e6d20d7a267f4780dd668c5fc9bb7cc7ded79b8
ashleighmjohnson <123028785+ashleighmjohnson@users.noreply.github.com>
2023-08-25 22:34:58 -0500

    Add files via upload

--- FiveAJohnson.java
+++ FiveAJohnson.java
@@ -5,12 +5,13 @@ import java.nio.file.Paths;
 public class FiveAJohnson {
     public static void main(String[] args) {
        
+        // changed numbers to decrypt
         int m = (int) Math.pow(2, 7);
         char[] cipher = new char[m];
         for (int i = 0; i < m; i++)
-            cipher[i] = (char)mod(7 * i + 3, m);
+        cipher [i] = (char ) mod(((i - 3) * 55), 128);
 
-        String plain = read("");
+        String plain = read("/Users/ashmi/Downloads/Data Structures - Fall23/Week 1 HW/cipher.txt");
         
         for (int i = 0; i < plain.length(); i++)
             System.out.print(cipher[plain.charAt(i)]);
